5c2123aa797326b185fa2d9e7e128ebf4b2c8f14
NITRO <NITRO@DESKTOP-H0G15DD>
2023-10-08 09:04:28 +0545

    Added schema

--- HilliHang.DataAccess/Data/ApplicationDbContext.cs
+++ HilliHang.DataAccess/Data/ApplicationDbContext.cs
@@ -13,6 +13,15 @@ namespace Bulky.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
 		public DbSet<ApplicationUser>ApplicationUsers { get; set; }
-  
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Category>().ToTable("categorie", schema: "inv");
+            builder.Entity<Product>().ToTable("products", schema: "inv");
+            builder.Entity<ApplicationUser>().ToTable("users", schema: "inv");
+        }
+
     }
 }
---
+++ HilliHang.DataAccess/Migrations/20231008031338_AddedSchema.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "StreetAddress",
+                table: "AspNetUsers");
+
+            migrationBuilder.EnsureSchema(
+                name: "inv");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "products",
+                newSchema: "inv");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "categorie",
+                newSchema: "inv");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CategoryId",
+                schema: "inv",
+                table: "products",
+                newName: "IX_products_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_products",
+                schema: "inv",
+                table: "products",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_categorie",
+                schema: "inv",
+                table: "categorie",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "inv",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    StreetAddress = table.Column<string>(type: "text", nullable: true),
+                    City = table.Column<string>(type: "text", nullable: true),
+                    State = table.Column<string>(type: "text", nullable: true),
+                    PostalCode = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_users_AspNetUsers_Id",
+                        column: x => x.Id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_categorie_CategoryId",
+                schema: "inv",
+                table: "products",
+                column: "CategoryId",
+                principalSchema: "inv",
+                principalTable: "categorie",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_categorie_CategoryId",
+                schema: "inv",
+                table: "products");
+
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "inv");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_products",
+                schema: "inv",
+                table: "products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_categorie",
+                schema: "inv",
+                table: "categorie");
+
+            migrationBuilder.RenameTable(
+                name: "products",
+                schema: "inv",
+                newName: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "categorie",
+                schema: "inv",
+                newName: "Categories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_products_CategoryId",
+                table: "Products",
+                newName: "IX_Products_CategoryId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreetAddress",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
